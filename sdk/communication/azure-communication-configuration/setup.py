662c9d9b9c9a09fe399a93b394a6b05ae90023f5
tural farhadov <tufarhad@microsoft.com>
2020-07-30 16:09:59 -0700

    Sync files with the template package

--- sdk/communication/azure-communication-configuration/setup.py
+++ sdk/communication/azure-communication-configuration/setup.py
@@ -1,68 +1,49 @@
-#!/usr/bin/env python
-
-#-------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
-#--------------------------------------------------------------------------
-
-import re
-import os.path
+from setuptools import setup, find_packages
+import os
 from io import open
-from setuptools import find_packages, setup
+import re
 
-# Change the PACKAGE_NAME only to change folder and different name
+# example setup.py Feel free to copy the entire "azure-template" folder into a package folder named 
+# with "azure-<yourpackagename>". Ensure that the below arguments to setup() are updated to reflect 
+# your package.
+
+# this setup.py is set up in a specific way to keep the azure* and azure-mgmt-* namespaces WORKING all the way 
+# up from python 2.7. Reference here: https://github.com/Azure/azure-sdk-for-python/wiki/Azure-packaging
+
 PACKAGE_NAME = "azure-communication-configuration"
 PACKAGE_PPRINT_NAME = "Communication Configuration Service"
 
 # a-b-c => a/b/c
-package_folder_path = PACKAGE_NAME.replace('-', '/')+"/_generated"
-package_folder_path_r = PACKAGE_NAME.replace('-', '/')
-print("curr folder", package_folder_path)
+package_folder_path = PACKAGE_NAME.replace('-', '/')
 # a-b-c => a.b.c
 namespace_name = PACKAGE_NAME.replace('-', '.')
 
-# azure v0.x is not compatible with this package
-# azure v0.x used to have a __version__ attribute (newer versions don't)
-try:
-    import azure
-    try:
-        ver = azure.__version__
-        raise Exception(
-            'This package is incompatible with azure=={}. '.format(ver) +
-            'Uninstall it with "pip uninstall azure".'
-        )
-    except AttributeError:
-        pass
-except ImportError:
-    pass
-
 # Version extraction inspired from 'requests'
-print(package_folder_path_r + '/_version.py')
-with open(package_folder_path_r + '/_version.py', 'r') as fd:
+with open(os.path.join(package_folder_path, '_version.py'), 'r') as fd:
     version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
-
 if not version:
     raise RuntimeError('Cannot find version information')
 
 with open('README.md', encoding='utf-8') as f:
-    readme = f.read()
-with open('CHANGELOG.md', encoding='utf-8') as f:
-    changelog = f.read()
+    long_description = f.read()
 
 setup(
     name=PACKAGE_NAME,
     version=version,
     description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
-    long_description=readme + '\n\n' + changelog,
-    long_description_content_type='text/markdown',
-    license='MIT License',
-    author='Microsoft Corporation',
-    author_email='azpysdkhelp@microsoft.com',
+
+    # ensure that these are updated to reflect the package owners' information
+    long_description=long_description,
     url='https://github.com/Azure/azure-sdk-for-python',
+    author='Microsoft Corporation',
+    author_email='azuresdkengsysadmins@microsoft.com',
+
+    license='MIT License',
+    # ensure that the development status reflects the status of your package
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
@@ -73,13 +54,21 @@ setup(
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
-    zip_safe=False,
     packages=find_packages(exclude=[
+        'tests',
+        # Exclude packages that will be covered by PEP420 or nspkg
+        'azure'
     ]),
     install_requires=[
-        'msrest>=0.5.0',
-
+        #'msrest>=0.5.0',
+        #'msrestazure>=0.4.32,<2.0.0',
+        #'azure-common~=1.1',
     ],
     extras_require={
+        ":python_version<'3.0'": ['azure-nspkg'],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/Azure/azure-sdk-for-python/issues',
+        'Source': 'https://github.com/Azure/azure-sdk-python',
     }
 )
---
+++ sdk/communication/azure-communication-configuration/tests/test_simple.py
@@ -0,0 +1,11 @@
+# Testing Azure Packages has some additional complication/reading required.
+# Reference https://github.com/Azure/azure-sdk-for-python/wiki/Contributing-to-the-tests
+# Pytest should be leveraged to test your project.
+
+from devtools_testutils import AzureMgmtTestCase
+
+# this test case highlights that there are some additional Test capabilities present in devtools_testutils
+# as a package owner you are not required to use these. Standard PyTest implementation will work.
+class TemplateTest(AzureMgmtTestCase):
+    def setUp(self):
+        super(TemplateTest, self).setUp()
