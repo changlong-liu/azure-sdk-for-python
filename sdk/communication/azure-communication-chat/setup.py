1d070811217426f31d3afcfb4b30b1f2898e3d44
Fang Chen <fanche@microsoft.com>
2020-08-06 14:33:51 -0700

    refactor setup.py, refer to azure-synapse-spark module

--- sdk/communication/azure-communication-chat/setup.py
+++ sdk/communication/azure-communication-chat/setup.py
@@ -11,7 +11,7 @@ import re
 # up from python 2.7. Reference here: https://github.com/Azure/azure-sdk-for-python/wiki/Azure-packaging
 
 PACKAGE_NAME = "azure-communication-chat"
-PACKAGE_PPRINT_NAME = "Azure Communication Chat"
+PACKAGE_PPRINT_NAME = "Communication Chat"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
@@ -26,24 +26,23 @@ if not version:
     raise RuntimeError('Cannot find version information')
 
 with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
+    readme = f.read()
+
+with open('CHANGELOG.md', encoding='utf-8') as f:
+    changelog = f.read()
 
 setup(
     name=PACKAGE_NAME,
     version=version,
     description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
-
-    # ensure that these are updated to reflect the package owners' information
-    long_description=long_description,
-    url='https://github.com/Azure/azure-sdk-for-python',
-    author='Microsoft Corporation',
-    author_email='azuresdkengsysadmins@microsoft.com',
-
+    long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/markdown',
     license='MIT License',
-    # ensure that the development status reflects the status of your package
+    author='Microsoft Corporation',
+    author_email='azpysdkhelp@microsoft.com',
+    url='https://github.com/Azure/azure-sdk-for-python',
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
@@ -52,8 +51,10 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
     ],
+    zip_safe=False,
     packages=find_packages(exclude=[
         'tests',
         # Exclude packages that will be covered by PEP420 or nspkg
@@ -68,9 +69,5 @@ setup(
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg'],
         ":python_version<'3.5'": ["typing"]
-    },
-    project_urls={
-        'Bug Reports': 'https://github.com/Azure/azure-sdk-for-python/issues',
-        'Source': 'https://github.com/Azure/azure-sdk-python',
     }
 )
