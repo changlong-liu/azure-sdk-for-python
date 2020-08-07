f1f44be409fa9c9bae308986b6fad9e7393985c4
tural farhadov <tufarhad@microsoft.com>
2020-08-07 15:11:53 -0700

    Add test placeholder for UserManagmentClient

--- sdk/communication/azure-communication-configuration/tests/test_simple.py
+++ sdk/communication/azure-communication-configuration/tests/test_simple.py
@@ -3,9 +3,20 @@
 # Pytest should be leveraged to test your project.
 
 from devtools_testutils import AzureMgmtTestCase
+import unittest
+import pytest
 
 # this test case highlights that there are some additional Test capabilities present in devtools_testutils
 # as a package owner you are not required to use these. Standard PyTest implementation will work.
-class TemplateTest(AzureMgmtTestCase):
+# a placeholder for UserManagementTests.
+# TODO: add tests
+class UserManagementTest(AzureMgmtTestCase):
     def setUp(self):
-        super(TemplateTest, self).setUp()
+        super(UserManagementTest, self).setUp()
+
+    def test_sample(self):
+        self.assertEqual(1,1)
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    unittest.main()
