c7951ae068b7bda795d437fa92b86740b59b5eee
tural farhadov <tufarhad@microsoft.com>
2020-08-19 14:49:59 -0700

    Add common package

---
+++ sdk/communication/azure-communication-common/azure/communication/common/models.py
@@ -0,0 +1,11 @@
+class CommunicationUser:
+    def __init__(self, userid):
+        self.userid = userid
+
+class PhoneNumber:
+    def __init__(self, value):
+        self.value = value
+
+class UnknownIdentifier:
+    def __init__(self, identifier):
+        self.identifier = identifier
