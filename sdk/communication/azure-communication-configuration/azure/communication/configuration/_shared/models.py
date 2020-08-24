842fc388db95cb50cb30ecd786abce8aaf0a7201
tural farhadov <tufarhad@microsoft.com>
2020-08-24 10:52:21 -0700

    Add documentation to the model classes

--- sdk/communication/azure-communication-configuration/azure/communication/configuration/_shared/models.py
+++ sdk/communication/azure-communication-configuration/azure/communication/configuration/_shared/models.py
@@ -11,13 +11,23 @@ class CommunicationIdentifier:
 class CommunicationUser(CommunicationIdentifier):
     """
     Represents a user in Azure Communication Service.
+
+    :ivar identifier: Communication user identifier.
+    :vartype identifier: str
+    :param identifier: Identifier to initialize CommunicationUser.
+    :type identifier: str
     """
-    def __init__(self, userid):
-        self.userid = userid
+    def __init__(self, identifier):
+        self.identifier = identifier
 
 class PhoneNumber(CommunicationIdentifier):
     """
     Represents a phone number.
+
+    :ivar value: Value for a phone number.
+    :vartype value: str
+    :param value: Value to initialize PhoneNumber.
+    :type value: str
     """
     def __init__(self, value):
         self.value = value
@@ -27,6 +37,11 @@ class UnknownIdentifier(CommunicationIdentifier):
     Represents an identifier of an unknown type.
     It will be encountered in communications with endpoints that are not
     identifiable by this version of the SDK.
+
+    :ivar identifier: Unknown communication identifier.
+    :vartype identifier: str
+    :param identifier: Value to initialize UnknownIdentifier.
+    :type identifier: str
     """
     def __init__(self, identifier):
         self.identifier = identifier
